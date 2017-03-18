9fcb22299ef376d11db6ffc4bdea35e4e12fe252
Abdul Bagasra <abdulbagasra@gmail.com>
2017-03-18 16:09:06 -0500

    Add dynamic update & delete functionality

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -10,6 +10,8 @@ class CommentBox extends Component {
     this.state = {data: []};
     this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
     this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    this.handleCommentDelete = this.handleCommentDelete.bind(this);
+    this.handleCommentUpdate = this.handleCommentUpdate.bind(this);
   }
 
   loadCommentsFromServer() {
@@ -20,19 +22,33 @@ class CommentBox extends Component {
   }
 
   handleCommentSubmit(comment) {
-    // let comments = this.state.data;
-    // comment.id = Date.now();
-    // let newComments = comments.concat([comment]);
-    // this.setState({data: newComments});
+    let comments = this.state.data;
+    comment.id = Date.now();
+    let newComments = comments.concat([comment]);
+    this.setState({data: newComments});
 
     axios.post(this.props.url, comment)
-      .then(res => {
-        this.setState({data: res});
-      })
       .catch(err => {
         console.error(err);
-        //this.setState({data: comments});
+        this.setState({data: comments});
       });
+  }
+
+  handleCommentDelete(id) {
+    axios.delete(`${this.props.url}/${id}`)
+         .then(res => {
+           console.log('Comment deleted');
+         })
+         .catch(err => {
+           console.error(err);
+    });
+  }
+
+  handleCommentUpdate(id, comment) {
+    axios.put(`${this.props.url}/${id}`, comment)
+         .catch(err => {
+           console.error(err);
+    });
   }
 
   componentDidMount() {
@@ -43,8 +59,9 @@ class CommentBox extends Component {
   render() {
     return (
       <div style={style.commentBox}>
-        <h2>Comments:</h2>
-        <CommentList data={this.state.data}/>
+        <h2 style={style.title}>Comments:</h2>
+        <CommentList onCommentDelete={this.handleCommentDelete}
+          onCommentUpdate={this.handleCommentUpdate} data={this.state.data}/>
         <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     )
