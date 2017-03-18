882ca19a7ff1e5d651c8578ab8d5b1a52401f517
Abdul Bagasra <abdulbagasra@gmail.com>
2017-03-18 01:37:01 -0500

    Begin implementing server-side code

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var app = express();
+var router = express.Router();
+var port = process.env.API_PORT || 3001;
+
+// db config
+mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_PASS);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Credentials', 'true');
+ res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+ res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+ res.setHeader('Cache-Control', 'no-cache');
+ next();
+});
+
+router.get('/', (req, res) => {
+  res.json({message: 'API Initialized!'});
+});
+
+app.use('/api', router);
+
+app.listen(port, () => {
+  console.log(`API running on port ${port}`);
+});
